b4700914de16b1b6da68610c7a89acccb7b9a6d8
ankar <caarevalo@unal.edu.co>
2021-12-05 05:42:18 +0300

    Cambios necesarios para el despliegue

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+cons app = express();
+
+//Serve only the static files form the dist directory
+app.use(express.static('./dist/frontend'));
+
+app.get('/*', (req, res) => res.sendFile("index.html", {root: 'dist/frontend/'}),);
+
+//Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080):
