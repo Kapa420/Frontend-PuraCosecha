5f3eba3790097b5ec2f6388e59a777fdb752f320
ankar <caarevalo@unal.edu.co>
2021-12-05 06:04:31 +0300

    Cambios en el server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
-//Serve only the static files form the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/frontend'));
 
-app.get('/', (req, res) => res.sendFile("index.html", {root: 'dist/frontend/'}),);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
+);
 
-//Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080):
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
